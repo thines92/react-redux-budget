2b67181c4b8d19cd2e12d1929b551989d26a794f
Tyler <tylerhines1992@gmail.com>
2020-06-02 15:16:31 -0400

    add redux-form imports to addCategory

--- src/components/addCategory.js
+++ src/components/addCategory.js
@@ -1,54 +1,69 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 class AddCategory extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            type: '',
-            source: ''
-        }
+    this.state = {
+      type: "",
+      source: "",
+    };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-    handleSubmit = (e) => {
-        console.log('e', e.target)
-        e.preventDefault();
-        console.log('props', this.props)
-        this.props.addCategory(this.state.type, this.state.source)
+  handleSubmit = (e) => {
+    console.log("e", e.target);
+    e.preventDefault();
+    console.log("props", this.props);
+    this.props.addCategory(this.state.type, this.state.source);
 
-        this.setState({
-            type: '',
-            source: ''
-        })
-    }
+    this.setState({
+      type: "",
+      source: "",
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit.bind(this)} className="ui form">
-                    <h4 className="ui dividing header">Add Category</h4>
-                    <div className="field">
-                        <div className="two fields">
-                            <div className="field">
-                                <input type="text" name="type" value={this.state.type} placeholder="Type" onChange={this.handleChange} />
-                            </div>
-                            <div className="field">
-                                <input type="text" name="source" value={this.state.source} placeholder="Source" onChange={this.handleChange} />
-                            </div>
-                        </div>
-                    </div>
-
-                    <button type="submit" className="ui button">Submit</button>
-                </form>
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit.bind(this)} className="ui form">
+          <h4 className="ui dividing header">Add Category</h4>
+          <div className="field">
+            <div className="two fields">
+              <div className="field">
+                <input
+                  type="text"
+                  name="type"
+                  value={this.state.type}
+                  placeholder="Type"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="field">
+                <input
+                  type="text"
+                  name="source"
+                  value={this.state.source}
+                  placeholder="Source"
+                  onChange={this.handleChange}
+                />
+              </div>
             </div>
-        )
-    }
+          </div>
+
+          <button type="submit" className="ui button">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddCategory;
