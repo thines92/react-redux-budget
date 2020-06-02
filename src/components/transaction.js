974c209afcf107736b869b3d4b4b75fe92c973b4
Tyler <tylerhines1992@gmail.com>
2020-06-02 17:00:29 -0400

    added redux-form to Transaction

--- src/components/transaction.js
+++ src/components/transaction.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
 class Transaction extends React.Component {
+    editTransaction = (props) => {
+        console.log('props', props)
+        return (
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.props.editTransaction)}>
+                <Field name="type" component={this.renderInput} label="Type" />
+                <Field
+                    name="source"
+                    component={this.renderInput}
+                    label="Source"
+                />
+                <button className="ui button primary">Save</button>
+            </form>
+        );
+    };
+
+    renderInput = ({ input, label, meta }) => {
+        console.log(label)
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    
+        return (
+          <div className={className}>
+            <label>{label}</label>
+            <input {...input} autoComplete="off" />
+            {this.renderError(meta)}
+          </div>
+        );
+      };
 
     render() {
         const { id, type, source } = this.props.transaction;
@@ -9,11 +37,24 @@ class Transaction extends React.Component {
             <tr>
                 <td datalabel="type">{type}</td>
                 <td datalabel="source">{source}</td>
-                <td datalabel="edit"><button className="ui button primary">Edit</button></td>
-                <td datalabel="delete"><button className="ui button red" onClick={this.props.deleteTransaction.bind(this, id)}>Delete</button></td>
+                <td datalabel="edit">
+                    <button className="ui button primary" onClick={this.editTransaction.bind(this, this.props.transaction)}>Edit</button>
+                </td>
+                <td datalabel="delete">
+                    <button
+                        className="ui button red"
+                        onClick={this.props.deleteTransaction.bind(this, id)}
+                    >
+                        Delete
+                    </button>
+                </td>
             </tr>
-        )
+        );
     }
 }
 
-export default Transaction;
+export default reduxForm({
+    form: 'editTransaction'
+})(Transaction)
+
+// export default Transaction;
--- src/components/transactionTable.js
+++ src/components/transactionTable.js
@@ -1,71 +1,73 @@
 import React from "react";
 import {
-  fetchTransactions,
-  addTransaction,
-  deleteTransaction,
+    fetchTransactions,
+    addTransaction,
+    deleteTransaction,
 } from "../actions/transactionActions";
 import { connect } from "react-redux";
 import TransactionForm from "./TransactionForm";
 import Transaction from "./Transaction";
 
 class TransactionTable extends React.Component {
-  componentDidMount = () => {
-    this.props.fetchTransactions();
-  };
+    componentDidMount = () => {
+        this.props.fetchTransactions();
+    };
 
-  renderTransactions = () => {
-    return this.props.transactions.map((transaction, i) => {
-      return (
-        <Transaction
-          key={transaction.id.toString()}
-          transaction={transaction}
-          deleteTransaction={this.handleDeleteTransaction}
-        />
-      );
-    });
-  };
+    renderTransactions = () => {
+        return this.props.transactions.map((transaction, i) => {
+            return (
+                <Transaction
+                    key={transaction.id.toString()}
+                    transaction={transaction}
+                    deleteTransaction={this.handleDeleteTransaction}
+                    editTransaction={this.handleEditTransaction}
+                />
+            );
+        });
+    };
 
-  handleDeleteTransaction = (id) => {
-    this.props.deleteTransaction(id);
-  };
+    handleDeleteTransaction = (id) => {
+        this.props.deleteTransaction(id);
+    };
 
-  handleAddTransaction = (type, source) => {
-    this.props.addTransaction({
-      id: this.props.transactions.length,
-      type: type,
-      source: source,
-    });
-  };
+    handleAddTransaction = (type, source) => {
+        this.props.addTransaction({
+            id: this.props.transactions.length,
+            type: type,
+            source: source,
+        });
+    };
 
-  handleEditTransaction = (transaction) => {
-    return;
-  };
+    handleEditTransaction = (transaction) => {
+        console.log(transaction);
+        return;
+    };
 
-  render() {
-    return (
-      <div className="ui container stackable">
-        <TransactionForm
-          addTransaction={this.handleAddTransaction.bind(this)}
-        />
-        <table className="ui two column celled table">
-          <thead>
-            <tr>
-              <th>Type</th>
-              <th>Source</th>
-            </tr>
-          </thead>
-          <tbody>{this.renderTransactions()}</tbody>
-        </table>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="ui container stackable">
+                <TransactionForm
+                    addTransaction={this.handleAddTransaction.bind(this)}
+                />
+                <table className="ui two column celled table">
+                    <thead>
+                        <tr>
+                            <th>Type</th>
+                            <th>Source</th>
+                        </tr>
+                    </thead>
+                    <tbody>{this.renderTransactions()}</tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return state.transactions;
+    return state.transactions;
 };
 export default connect(mapStateToProps, {
-  fetchTransactions,
-  addTransaction,
-  deleteTransaction,
+    fetchTransactions,
+    addTransaction,
+    deleteTransaction,
 })(TransactionTable);
