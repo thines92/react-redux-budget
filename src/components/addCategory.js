df1a6ce56b4c3c7ed052e2cba2bbacf575cfa4dc
Tyler <tylerhines1992@gmail.com>
2020-06-02 15:26:35 -0400

    add basic redux-form to addCategory

--- src/components/addCategory.js
+++ src/components/addCategory.js
@@ -9,55 +9,46 @@ class AddCategory extends React.Component {
       type: "",
       source: "",
     };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  onSubmit = (e) => {
-    console.log("e", e.target);
-    e.preventDefault();
-    console.log("props", this.props);
-    this.props.addCategory(this.state.type, this.state.source);
+  renderInput = (props) => {
+    console.log(props);
+    return (
+      <div className="field">
+        <label>{props.label}</label>
+        <input {...props.input} autoComplete="off" />
+      </div>
+    );
+  };
 
-    this.setState({
-      type: "",
-      source: "",
-    });
+  onSubmit = ({ type, source }) => {
+    this.props.addCategory(type, source);
   };
 
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+          className="ui form error"
+        >
           <h4 className="ui dividing header">Add Category</h4>
           <div className="field">
             <div className="two fields">
               <div className="field">
-                <input
-                  type="text"
-                  name="type"
-                  value={this.state.type}
-                  placeholder="Type"
-                  onChange={this.handleChange}
-                />
+                <Field name="type" component={this.renderInput} label="Type" />
               </div>
               <div className="field">
-                <input
-                  type="text"
-                  name="source"
-                  value={this.state.source}
-                  placeholder="Source"
-                  onChange={this.handleChange}
-                />
+                  <Field name="source" component={this.renderInput} label="Source" />
               </div>
             </div>
           </div>
 
-          <button type="submit" className="ui button">
+          <button className="ui button primary">
             Submit
           </button>
         </form>
@@ -67,5 +58,5 @@ class AddCategory extends React.Component {
 }
 
 export default reduxForm({
-    form: 'addCategory'
+  form: "addCategory",
 })(AddCategory);
