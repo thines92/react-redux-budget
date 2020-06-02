937f0d513124a683ad5441baf1a4bbd47aaff830
Tyler <tylerhines1992@gmail.com>
2020-06-02 16:07:14 -0400

    created delete_transaction reducer

--- src/reducers/transactionReducer.js
+++ src/reducers/transactionReducer.js
@@ -1,18 +1,31 @@
-import { items } from '../staticItems'
+import { items } from "../staticItems";
 
-export default (state = {
+export default (
+  state = {
     transactions: items,
-    error: null
-}, action) => {
-    switch (action.type) {
-        case "FETCH_TRANSACTIONS": {
-            return { ...state }
-        }
-        case "ADD_TRANSACTION": {
-            console.log('payload', action.payload)
-            return { ...state, transactions: [...state.transactions, action.payload] };
-        }
-        default:
-            return state;
+    error: null,
+  },
+  action
+) => {
+  switch (action.type) {
+    case "FETCH_TRANSACTIONS": {
+      return { ...state };
     }
-}
+    case "ADD_TRANSACTION": {
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload],
+      };
+    }
+    case "DELETE_TRANSACTION": {
+        console.log('delete_transaction payload', action.payload)
+        const filteredTransactions = state.transactions.filter(id => id == action.payload);
+        return {
+            ...state,
+            filteredTransactions
+        }
+    }
+    default:
+      return state;
+  }
+};
